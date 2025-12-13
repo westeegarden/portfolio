f16d95c80b574684ff158fe0dcb832d457c2b8ab
westeegarden <160755607+westeeg@users.noreply.github.com>
2025-12-13 14:40:43 -0500

    Complete contact page w/ live links

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,30 @@
+import { MdEmail, MdSchool } from "react-icons/md";
+import { FaLinkedin, FaHandshake } from "react-icons/fa";
+
+
 export default function Contact() {
   return (
-    <div>
-      <h2>Contact me</h2>
-      <p>This is the contact page of my portfolio.</p>
+    <div className="contact-page">
+        <h2 className="contact-header">Contact Me</h2>
+
+        <div className="contact-links">
+            <a href="mailto:wesley.teegarden@uvm.edu">
+                School Email
+                <MdSchool size={24} /> 
+            </a>
+            <a href="mailto:westeegarden@gmail.com">
+                Gmail
+                <MdEmail size={24} /> 
+            </a>
+            <a href="https://www.linkedin.com/in/wesley-teegarden-a53462267/" target="_blank" rel="noopener noreferrer">
+                LinkedIn
+                <FaLinkedin size={24} /> 
+            </a>
+            <a href="https://app.joinhandshake.com/profiles/mcj9uu" target="_blank" rel="noopener noreferrer">
+                Handshake
+                <FaHandshake size={24} /> 
+            </a>
+        </div>
     </div>
   );
 }
